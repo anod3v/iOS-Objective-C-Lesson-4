ec6725caf66c953d40352c473c177d53b7dcbb81
Anoshkin <a.anoshkin@profsoft.pro>
2021-02-10 07:49:49 +0300

    basic init done

--- Lesson4/Lesson4/Student.h
+++ Lesson4/Lesson4/Student.h
@@ -5,9 +5,15 @@
 //  Created by Andrey on 10/02/2021.
 //
 
+
 #import <Foundation/Foundation.h>
+
+@class Student;
 
 @interface Student : NSObject
 
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *surname;
+@property (strong, nonatomic) NSNumber *age;
 
 @end
--- Lesson4/Lesson4/Student.m
+++ Lesson4/Lesson4/Student.m
@@ -5,45 +5,22 @@
 //  Created by Andrey on 10/02/2021.
 //
 
+
 #import "Student.h"
-
-enum PublicationType {
-    PublicationTypeNews,
-    PublicationTypeAnnouncement,
-    PublicationTypeArticle
-};
-
-typedef enum PublicationType PublicationType;
-
-@interface Student()
-
-@property (nonatomic, strong) NSString *title;
-@property (nonatomic, strong) NSString *text;
-@property (nonatomic, strong) NSDate *date;
-@property (nonatomic) PublicationType type;
-
-- (void)print;
-
-@end
 
 @implementation Student
 
-- (void)print {
-    NSLog(@"Title = %@ \nText = %@ \nDate = %@ \nType = %@", self.title, self.text, self.date, [self typeNameFrom:self.type]);
-}
-
-- (NSString *)typeNameFrom:(PublicationType)type {
-    switch (type) {
-        case PublicationTypeNews:
-            return @"Новость";
-            break;
-        case PublicationTypeAnnouncement:
-            return @"Объявление";
-            break;
-        case PublicationTypeArticle:
-            return @"Статья";
-            break;
+- (instancetype)initWithName:(NSString *)name
+                     surname:(NSString *)surname
+                         age:(NSNumber *)age {
+    
+    self = [super init];
+    if (self) {
+        _name = name;
+        _surname = surname;
+        _age = age;
     }
+    return self;
 }
-
 @end
+
